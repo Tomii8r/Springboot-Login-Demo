907baa787bd043861229598e2bae185a0b795286
tomii8r <ttomaserazo@gmail.com>
2026-05-19 17:20:35 -0300

    Validacion de logintud y medidas seguridad en contraseñas en el registro de usuario

--- src/main/java/com/example/demoInicioSesion/controllers/UsuarioController.java
+++ src/main/java/com/example/demoInicioSesion/controllers/UsuarioController.java
@@ -3,10 +3,12 @@ package com.example.demoInicioSesion.controllers;
 import com.example.demoInicioSesion.dao.UsuarioDao;
 import com.example.demoInicioSesion.models.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -38,11 +40,39 @@ public class UsuarioController {
     }
 
     @PostMapping("/registro")
-    public String registrarUsuario(Usuario usuario) {
+    public String registrarUsuario(Usuario usuario, RedirectAttributes redirectAttributes) {
+
+
+        if (usuarioDao.existsByUsername(usuario.getUsername())) {
+
+            redirectAttributes.addFlashAttribute("estado", "Nombre de usuario ya existe");
+
+            return "redirect:/registro";
+        }
+        if(usuario.getUsername() == null || usuario.getUsername().trim().isEmpty() || usuario.getPassword() == null || usuario.getPassword().trim().isEmpty()){
+            redirectAttributes.addFlashAttribute("estado", "Rellene todos los campos");
+            return "redirect:/registro";
+        }
+        if(usuario.getUsername().length() < 3 || usuario.getUsername().length() > 25 ){
+            redirectAttributes.addFlashAttribute("estado", "Usuario debe contener entre 3 y 25 caracteres");
+            return "redirect:/registro";
+        }
+        if (usuario.getPassword().length() < 6 || usuario.getPassword().length() > 32){
+            redirectAttributes.addFlashAttribute("estado","La contraseña debe contener entre 6 y 32 caracteres");
+            return "redirect:/registro";
+        }
+        if(!usuario.getPassword().matches("^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).+$")){
+            redirectAttributes.addFlashAttribute("estado","La contraseña debe contenar una mayuscula y al menos un caracter especial");
+            return "redirect:/registro";
+        }
 
         usuarioDao.save(usuario);
 
-        return "redirect:/";
+        redirectAttributes.addFlashAttribute("estado", "Usuario registrado con éxito");
+        return "redirect:/registro";
     }
-
+    @GetMapping("/registro")
+    public String mostrarRegistro() {
+        return "registro";
+    }
 }
--- src/main/java/com/example/demoInicioSesion/dao/UsuarioDao.java
+++ src/main/java/com/example/demoInicioSesion/dao/UsuarioDao.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Integer> {
     Usuario findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
