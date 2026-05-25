d9c5a55024fd3631b186a0636fad94a893127f19
tomii8r <ttomaserazo@gmail.com>
2026-05-25 10:53:16 -0300

    Saludo al iniciar sesion

--- src/main/java/com/example/demoInicioSesion/controllers/UsuarioController.java
+++ src/main/java/com/example/demoInicioSesion/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.example.demoInicioSesion.controllers;
 import com.example.demoInicioSesion.dao.UsuarioDao;
 import com.example.demoInicioSesion.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,7 +40,9 @@ public class UsuarioController {
     }
 
     @GetMapping("/success")
-    public String success() {return "success";
+    public String success(Model model, Authentication authentication) {
+        model.addAttribute("saludo", "Hola "+ authentication.getName());
+        return "success";
     }
 
     @PostMapping("/registro")
